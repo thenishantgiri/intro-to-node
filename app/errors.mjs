7ccab2f7e50ed02ace8ade7753ada6be84786101
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-10 04:43:16 +0530

    errors: throw

--- app/errors.mjs
+++ app/errors.mjs
@@ -2,6 +2,6 @@ import { readFile } from "fs/promises";
 try {
   const result = await readFile(new URL("app.mjs", import.meta.url), "utf-8");
 } catch (err) {
-  console.error(err);
-  console.log("yay! our code didn't break");
+  throw err;
+  console.log("oops! our app crashed!");
 }

49524a4bf6e61801520ed7ba22dd2cc81eddf0bb
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-10 04:50:01 +0530

    errors: process.on('uncaughtException', ()=>{}

--- app/errors.mjs
+++ app/errors.mjs
@@ -1,7 +1,11 @@
 import { readFile } from "fs/promises";
-try {
-  const result = await readFile(new URL("app.mjs", import.meta.url), "utf-8");
-} catch (err) {
-  throw err;
-  console.log("oops! our app crashed!");
-}
+
+process.on("uncaughtException", (err) => {
+  console.error(err);
+});
+
+const result = await readFile(new URL("app.mjs", import.meta.url), "utf-8");
+
+console.log(
+  "damn! another app crash...'process.on' exits out of node application"
+);

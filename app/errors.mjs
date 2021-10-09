5f1aa733375a5689d1354048dc8a39359dbfc182
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-10 04:37:06 +0530

    errors: promises

--- app/errors.mjs
+++ app/errors.mjs
@@ -1,10 +1,7 @@
-import { readFile } from "fs";
-
-readFile(new URL("app.mjs", import.meta.url), "utf-8", (err, data) => {
-    if (err) {
-        //handle the error
-        console.error(err);
-    } else {
-        //do the task
-    }
- });
+import { readFile } from "fs/promises";
+try {
+  const result = await readFile(new URL("app.mjs", import.meta.url), "utf-8");
+} catch (err) {
+  console.error(err);
+  console.log("yay! our code didn't break");
+}

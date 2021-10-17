d6274e232af903110f1da3ad1e487f324f788d02
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-10-17 07:15:45 +0530

    express server

---
+++ app/server/server.mjs
@@ -0,0 +1,38 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+const db = [];
+
+app.post("/todo", (req, res) => {
+  const newTodo = {
+    id: Date.now(),
+    text: req.body.text,
+  };
+
+  db.push(newTodo);
+  res.json(newTodo);
+});
+
+app.get("/todo", (req, res) => {
+  res.json(db);
+});
+
+app.get("/todo/:id", (req, res) => {
+  const todo = db.find((todo) => {
+    return todo.id === +req.params.id;
+  });
+
+  res.json({
+    data: todo,
+  });
+});
+
+app.listen(8000, () => {
+  console.log("server is listening on http://localhost:8000");
+});
